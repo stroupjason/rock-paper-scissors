f4fa75da3bac6ec72a90c67dc98fe052263e9ec6
Jason Stroup <jasonstroup12@gmail.com>
2019-04-08 19:33:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("yo")
+
+//cache the vDom - storing for future use
+
+const UserSCore = 0;
+const computerScore = 0;
+const userScore_span = docment.getElementById("user-score");
+const computerScore_span = docment.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r")
+const paper_div = document.getElementById("p")
+const scissors_div = document.getElementById("s")
+
+// video left off at 31:43
+
